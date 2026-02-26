41a20b902cda401f6c46324cac02c7e1bf2cfbf6
Dinesh kumaran <dineshkumarank2378@gmail.com>
2026-02-26 16:32:44 +0530

    Examples

---
+++ Examples/Example2.c
@@ -0,0 +1,27 @@
+ 
+ #pragma config FOSC = HS     // Oscillator Selection bits (RC oscillator)
+#pragma config WDTE = OFF       // Watchdog Timer Enable bit (WDT disabled)
+#pragma config PWRTE = OFF      // Power-up Timer Enable bit (PWRT disabled)
+#pragma config BOREN = OFF      // Brown-out Reset Enable bit (BOR disabled)
+#pragma config LVP = OFF        // Low-Voltage (Single-Supply) In-Circuit Serial Programming Enable bit (RB3 is digital I/O, HV on MCLR must be used for programming)
+#pragma config CPD = OFF        // Data EEPROM Memory Code Protection bit (Data EEPROM code protection off)
+#pragma config WRT = OFF        // Flash Program Memory Write Enable bits (Write protection off; all program memory may be written to by EECON control)
+#pragma config CP = OFF         // Flash Program Memory Code Protection bit (Code protection off)
+
+
+#include "D:\Learning Doc\PIC Learning\Header\GPIO\gpio.h"
+#include "D:\Learning Doc\PIC Learning\Middleware\sevensegment\sevseg.h"
+#include "D:\Learning Doc\PIC Learning\Middleware\keypad\keypad.h"
+#include"D:\Learning Doc\PIC Learning\Header\Timer\timer.h"
+ 
+ void main ()
+    {
+
+        config_port(PORTB,output);
+        setportlow(PORTB);
+        while(1)
+        {
+            *PORTB^=0XFF;
+            delay(1000);
+        }
+    }
