08babf5d0715dbe1250e87944b7baddded7a0ba5
Dinesh kumaran <dineshkumarank2378@gmail.com>
2026-03-26 10:04:51 +0530

    Middleware for Lcd

--- Middleware/Lcd/lcd.c
+++ Middleware/Lcd/lcd.c
@@ -1,34 +1,56 @@
 
 #include"lcd.h" 
  
- void lcd_init(volatile unsigned char*a,volatile unsigned char *b, uint8_t e,uint8_t rs )//  for initialize the lcd
+
+
+static  volatile unsigned char *lcd_dport  ;
+static  volatile unsigned char * lcd_cport;
+static uint8_t  lcd_en;
+static uint8_t lcd_rs;
+
+
+
+ void lcd_config(volatile unsigned char*a,volatile unsigned char *b, uint8_t e,uint8_t rs )
     {
-        for(uint8_t i=0;i<20;i++);
+     delay_ms(20);
         lcd_dport=a;
         lcd_cport=b;
         lcd_en=e;
         lcd_rs=rs;
     }
-    void lcd_cmd(uint8_t a) // for command
+ 
+ void lcd_init()
+{
+    *lcd_cport&=~(1<<lcd_rs);  
+      delay_ms(20);   
+	lcd_cmd(0x38);
+	
+	lcd_cmd(0x0c);
+	
+	lcd_cmd(0x06);
+    
+    lcd_cmd(0x01);
+
+    lcd_cmd(0x80);      
+    delay_ms(3);
+	
+}
+void lcd_cmd(uint8_t a) // for command
     {
-        *lcd_cport&=(1<<lcd_rs);
+        *lcd_cport&=~(1<<lcd_rs);
         *lcd_dport=a;
-        *lcd_cport&=~(1<<lcd_en);
-        for(uint8_t t=0;t<10;t++)
-        *lcd_cport|=(1<<lcd_en);    
+         lcd_enable();   
         
     }
     
-    void lcd_data(char b) // For data Transfer
+void lcd_data(uint8_t b) // For data Transfer
     {
         *lcd_cport|=(1<<lcd_rs);
          *lcd_dport=b;
-        *lcd_cport&=~(1<<lcd_en);
-        for(uint8_t t=0;t<15;t++)
-        *lcd_cport|=(1<<lcd_en);
+         lcd_enable();
     }
      
-  void  lcd_out(char * c) // data to be transfered
+void  lcd_out(char * c) // data to be transfered
     {      
         for(uint16_t i=0;c[i]!='\0';i++)
         {
@@ -36,3 +58,13 @@
         }
     }
     
+void lcd_enable()
+  {
+     
+    *lcd_cport &= ~(1<<lcd_en);
+    delay_ms(1);
+    *lcd_cport |= (1<<lcd_en);
+    delay_ms(1);
+    *lcd_cport &= ~(1<<lcd_en);
+    delay_ms(2);      
+  }
--- Middleware/Lcd/lcd.h
+++ Middleware/Lcd/lcd.h
@@ -1,20 +1,17 @@
 #include "D:\Learning Doc\PIC Learning\Header\GPIO\gpio.h"
+#include"D:\Learning Doc\PIC Learning\Header\Timer\timer.h"
 
 #ifndef lcd_H
 #define lcd_H
 
 
-static  volatile unsigned char *lcd_dport  ;
-static  volatile unsigned char * lcd_cport;
-static uint8_t  lcd_en;
-static uint8_t lcd_rs;
 
-
-
- void lcd_init(volatile unsigned char*a,volatile unsigned char *b, uint8_t e,uint8_t rs );
+void lcd_init();
+void lcd_config(volatile unsigned char*a,volatile unsigned char *b, uint8_t e,uint8_t rs );
 void lcd_cmd(uint8_t a);
-void lcd_data(char b);
-void  lcd_out(char * c);
+void lcd_data(uint8_t b);
+void lcd_out(char * c);
+void lcd_enable();
 
    
     #endif 
